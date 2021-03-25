ea251ce0521b885afd4a117d94f7f86c208e0624
deepsi82 <deepsi82@gmail.com>
2021-03-24 20:18:01 -0500

    created package.json file and other js files to start the project and added some details

---
+++ account.js
@@ -0,0 +1,24 @@
+let userData =[
+    {
+        "ATMPin" : 1234,
+        "balance" : 140
+    },
+    {
+        "ATMPin" : 1310,
+        "balance" : 1300
+    },
+    {
+        "ATMPin" : 1312,
+        "balance" : 850
+    },
+    {
+        "ATMPin" : 4553,
+        "balance" : 2000
+    },
+    {
+        "ATMPin" : 0307,
+        "balance" : 900
+    },
+];        
+module.exports.data = userData;
+
---
+++ atm.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// function atm(pinNumber){
+//     let atmPin = promptFor("please enter your ATM pin number");
+//     let rightATMPin = pinNumber.filter() 
+    
+
+
+// }
+const account = require('./account')
